c2504301b48f4921d578977f794ad20477d70552
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2020-03-10 20:19:27 +0100

    Add onClick to RatingStar

--- frontend/components/rating/index.js
+++ frontend/components/rating/index.js
@@ -3,12 +3,17 @@ import PropTypes from "prop-types";
 import './style.scss';
 import Icons from '../icons';
 
-const Rating = ({iconName, maxRating, actualRating}) => {
+const Rating = ({iconName, maxRating, actualRating, onClick}) => {
+    const clickStar = () => {
+        if(onClick) {
+            onClick();
+        }
+    };
     const createIconsArray = ()=> {
         const iconsArray = [];
         for (let index = 0;index < maxRating; index++){
             let className = index < actualRating ?  'rating__icon rating__icon--active' : 'rating__icon'
-            let icon = <div className={className}><Icons name={iconName}/></div>
+            let icon = <div className={className} onClick={clickStar()}><Icons name={iconName} /></div>
             iconsArray.push(icon)
         }
         return iconsArray
