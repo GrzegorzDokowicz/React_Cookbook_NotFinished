b4ad7d4b24d5d811cb3f1548721e118c87b2fb56
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2020-04-05 19:39:21 +0200

    Added hooks to Loader component

--- frontend/app/components/page-elements/loader/index.js
+++ frontend/app/components/page-elements/loader/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Logo from '../../../../components/logo';
 
 //TODO add name to props and read if you should load data
 const Loader = ({isLoading = false, children}) => {
+    const [shouldRender, setRender] = useState(isLoading);
+    useEffect(() => {
+        if (isLoading) {
+            setRender(true);
+        }
+        return () => {
+            if (!isLoading) {
+                setTimeout(() => setRender(false), 3000);
+            }
+        };
+    }, [isLoading]);
 
-    const Load = () => {
-        return <Logo/>;
+
+    const Element = () => {
+        return <div className={'loader'}><Logo/></div>;
     };
 
+
     return <React.Fragment>
-        {isLoading ? <Load/> : children}
+        {shouldRender ? <Element/> : children}
     </React.Fragment>;
 };
 
